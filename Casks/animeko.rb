31b891a6f1fa93980be3ff7a3fcab448697d531a
ix4n33 <blackoutxen@gmail.com>
2026-03-31 02:20:32 +0800

    fix wrong url

--- Casks/animeko.rb
+++ Casks/animeko.rb
@@ -2,12 +2,14 @@ cask "animeko" do
     version "5.3.2"
 
     on_arm do
-      url "https://github.com/open-ani/animeko/releases/download/#{version}/ani-#{version}-macos-aarch64.dmg"
+      url "https://d.myani.org/v#{version}/ani-#{version}-macos-aarch64.dmg"
+      # url "https://github.com/open-ani/animeko/releases/download/v#{version}/ani-#{version}-macos-aarch64.dmg"
       sha256 "d3bac466fa7c8bb331beb279ffab0ab300508fbd26535fd01e8ae3bf700f7e7d"
     end
 
     on_intel do
-      url "https://github.com/open-ani/animeko/releases/download/#{version}/ani-#{version}-macos-x86_64.zip"
+      url "https://d.myani.org/v#{version}/ani-#{version}-macos-x86_64.zip"
+      # url "https://github.com/open-ani/animeko/releases/download/v#{version}/ani-#{version}-macos-x86_64.zip"
       sha256 "accbfbb439f9d92b15952ff74d8c6a8bd13dfa56735f3bd86db18267e3b650bd"
     end
 
--- Casks/animeko@beta.rb
+++ Casks/animeko@beta.rb
@@ -2,12 +2,14 @@ cask "animeko@beta" do
     version "5.4.0-beta05"
 
     on_arm do
-      url "https://github.com/open-ani/animeko/releases/download/#{version}/ani-#{version}-macos-aarch64.dmg"
+      url "https://d.myani.org/v#{version}/ani-#{version}-macos-aarch64.dmg"
+      # url "https://github.com/open-ani/animeko/releases/download/v#{version}/ani-#{version}-macos-aarch64.dmg"
       sha256 "58a24b08f02ef63aaf5a761107d6ea05d48052dcc4306cc7153aed9b53f459bb"
     end
 
     on_intel do
-      url "https://github.com/open-ani/animeko/releases/download/#{version}/ani-#{version}-macos-x86_64.zip"
+      url "https://d.myani.org/v#{version}/ani-#{version}-macos-x86_64.zip"
+      # url "https://github.com/open-ani/animeko/releases/download/v#{version}/ani-#{version}-macos-x86_64.zip"
       sha256 "f47a5870ecaaad25b8b2114ab4180dfd2fb33e1518e51a556238cacd1765572d"
     end
 
